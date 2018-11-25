e7e780ee8c17e0b17208809d12027a457646c9dd
QuantumSheep <nathanael.dmc@outlook.fr>
2018-11-25 17:50:31 +0100

    Improve website stability

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-const express = require('express');
-const path = require('path');
+const http = require('http');
+const handler = require('serve-handler');
 
-const app = express();
+const port = 3000;
 
-app.use('/assets', express.static(path.resolve('assets')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve('index.html'));
-});
-
-app.listen(3000, () => console.log('Server launched on http://localhost:3000!'));
+http.createServer(async (request, response) => await handler(request, response, { public: "static" }))
+  .listen(port, () => console.log(`Server launched on http://localhost:${port}`));
