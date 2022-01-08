4475ee38fdc352fe1b0c624a084d18c18198e645
quantumsheep <nathanael.dmc@outlook.fr>
2022-01-08 21:46:58 +0100

    Remove `gatsby-plugin-advanced-sitemap`

--- gatsby-config.js
+++ gatsby-config.js
@@ -175,7 +175,6 @@ module.exports = {
     },
     `gatsby-theme-material-ui`,
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-advanced-sitemap`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-image`,
     `gatsby-plugin-offline`,
